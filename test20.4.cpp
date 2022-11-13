7079adb72e92f64823ed562b97575fb2721173ef
ohjun <ohjun@umich.edu>
2022-11-13 17:51:21 -0500

    SUBMIT GOOOOOOOOOOOOOOOOOOOOO

--- test20.4.cpp
+++ test20.4.cpp
@@ -8,6 +8,39 @@
 #include <iostream>
 
 int main() {
+    /* Allocate swap-backed page from the arena */
+    char *filename1234 = (char *) vm_map(nullptr, 0);
+
+    /* Write the name of the file that will be mapped */
+    strcpy(filename1234, "lampson83.txt");
+
+    // Map a page from the specified file */
+    char *p01234 = (char *) vm_map (filename1234, 0);
+
+    /* Write the name of the file that will be mapped */
+    strcpy(filename1234, "data1.bin");
+
+    char *p11234 = (char *) vm_map (filename1234, 0);
+    char *p21234 = (char *) vm_map (filename1234, 0);
+    std::cout << p21234[0];
+    std::cout << p11234[0];
+    char *p31234 = (char *) vm_map (filename1234, 0);
+    std::cout << p31234[0];
+
+    vm_yield();
+
+    p31234[0] =  'b';
+
+    char* junk11234 = (char *) vm_map(nullptr, 0);
+    char* junk21234 = (char *) vm_map(nullptr, 0);
+    char* junk31234 = (char *) vm_map(nullptr, 0);
+
+    junk11234[0] = 'a';
+    junk21234[0] = 'b';
+    junk31234[0] = 'c';
+
+    std::cout << p31234[0];
+
     /* 4 pages of physical memory in the system */
     if (fork()) { // parent
         /* Allocate swap-backed page from the arena */
---
+++ test21.4.cpp
@@ -0,0 +1,50 @@
+// swap-basked: random test case
+
+#include "vm_app.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <cassert>
+#include <cstring>
+#include <iostream>
+
+int main() {
+    /* 4 pages of physical memory in the system */
+    if (fork()) { // parent
+        /* Allocate swap-backed page from the arena */
+        char *filename = (char *) vm_map(nullptr, 0);
+
+        /* Write the name of the file that will be mapped */
+        strcpy(filename, "lampson83.txt");
+
+        // Map a page from the specified file */
+        char *p0 = (char *) vm_map (filename, 0);
+
+        vm_yield();
+
+        char *swap0 = (char *) vm_map (nullptr, 0);
+        swap0[0] = 'a';
+        char *swap1 = (char *) vm_map (nullptr, 0);
+        swap1[0] = 'b';  // p0/c0 becomes unreferenced
+
+        vm_yield();
+    }
+    else { // child
+        char *filename_child = (char *) vm_map(nullptr, 0);
+        strcpy(filename_child, "lampson83.txt");
+
+        char* c0 = (char *) vm_map (filename_child, 0);
+
+        std::cout << c0[0];
+
+        vm_yield();
+
+        std::cout << c0[0];
+
+        char *swap_child0 = (char *) vm_map (nullptr, 0);
+        swap_child0[0] = 'x';
+        char *swap_child1 = (char *) vm_map (nullptr, 0);
+        swap_child1[0] = 'y';
+        char *swap_child2 = (char *) vm_map (nullptr, 0);
+        swap_child2[0] = 'z';
+    }
+}
